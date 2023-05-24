5487cb2230f24565ff78fb27a7331f7fffdd85b1
HerDonghyuk <idnosukja@naver.com>
2023-05-24 11:17:09 +0900

    230524

--- doit-js-main/08/js/00 js.js
+++ doit-js-main/08/js/00 js.js
@@ -1,2 +1,8 @@
-new Date("2025-05-25");
-new Date(2025, 2, 25);
+function changeBg() {
+  const bgCount = 5;
+
+  let randomNumber = Math.floor(Math.random() * bgCount) + 1;
+  document.body.style.backgroundImage = `url(images/bg-${randomNumber}.jpg)`;
+}
+
+document.addEventListener("load", changeBg());
