9fd84ef82237297e0c0268863ff5eaf847acc79a
Her Donghyuk <idnosukja@naver.com>
2023-05-26 08:00:02 +0900

    230526

--- doit-js-main/11/js/00 js.js
+++ doit-js-main/11/js/00 js.js
@@ -1,143 +1,55 @@
-/* function hello(name, message="안녕?") {
-  console.log(`${name}님, ${message}`)
-}
-hello("도레미", "반갑스비다");
-hello("백두산") */
-/* 
-function addNum(...numbers) {
-  let sum = 0;
+/* let hi = "hello";
 
-  for (let number of numbers) {
-    sum += number;
-
-  }
-  return sum;
+for (let ch of hi) {
+  console.log(ch);
 }
 
-console.log(addNum(1, 2));
-console.log(addNum(1, 2, 3, 4, 5)); */
+let chArray = [...hi];
+console.log(chArray);
 
-/* const animal = ["bird", "cat"];
-const fruits = ["apple", "banana", "cherry"]
-
-let sum1 = animal.concat(fruits)
-let sum2 = [...animal, ...fruits]
-
-console.log(sum1)
-console.log(sum2) */
-
-/* const fruits = ["apple", "banana", "cherry"]
-const favorite = fruits;
-favorite[1] = "grape";
-console.log(fruits);
- */
-/* const fruits = ["apple", "banana", "cherry"];
-const mine = [...fruits];
-mine[1] = "orange";
-console.log(fruits);
-console.log(mine); */
-
-/* 
-const book = {
-  title : "Javascript",
-  pages : 500
-}
-book["pubilshed date"] = "2022-01";
-console.log(book) */
-
-/* function fn() {
-  return "result"
-}
-const obj = {
-  [fn()] : "함수 키"
-}
-console.log(obj) */
-
-/* function fn() {
-  return "result"
-}
-
-function add(a, b) {
-  return a + b;
-}
-const obj = {
-  [fn()] : "함수 키",
-  [`${add(10, 20)} key`] : "계산식 키"
-}
-console.log(obj); */
-
-/* let user = {
-  name : "도레미"
-}
-
-user.age = 25
-console.log(user)
+let [ch1, ch2] = hi;
+console.log(ch1);
+console.log(ch2);
  */
 
-/* function makeUser(name, age) {
-  return {
-    name : name,
-    age : age
-  }
-}
-let user1 = makeUser("백두산", 20);
-console.log(user1) */
+/* let arr = [1, 2, 3, 4, 5]
+let it = arr[Symbol.iterator]()
+console.log(it)
 
-/* function makeUser(name, age) {
-  return {
-    name,
-    age : age
-  }
-}
-let user1 = makeUser("백두산", 20);
-console.log(user1) */
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next()) */
 
-/* let id1 = Symbol()
-let id2 = Symbol()
-console.log(id1 === id2) */
-
-/* 
-const id = Symbol("id")
-const tel = Symbol("telephone number")
-
-const member = {
-  name : "kim",
-  age : 25,
-  [id] : 1235,
-  [tel] : function() {
-    alert(prompt("전화번호 : "));
-  }
+function fnc() {
+  console.log("1");
+  console.log("2");
+  console.log("3");
 }
 
-for(item in member) {
-  console.log(`${item} : ${member[item]}`)
+fnc();
+
+function* gen() {
+  yield 1;
+  yield 2;
+  yield 3;
 }
 
-console.log(member[id])
-console.log(member[tel]()) */
+let g1 = gen();
+console.log(g1);
+console.log(g1);
+console.log(g1.next());
+console.log(g1.next());
+console.log(g1.next());
+console.log(g1.next());
+console.log(g1);
 
-/* let tel = Symbol.for("tel");
-let phone = Symbol.for("tel")
-console.log(tel === phone);
+let g2 = gen();
+for (let i of g2) console.log(i);
 
-let a = Symbol.keyFor(phone);
-console.log(a) */
-
-/* const fruits = ["사과", "복숭아"];
-// let apple = fruits[0];
-// let peach = fruits[1];
-let [apple, peach] = fruits
-console.log(fruits)
-console.log(apple) */
-
-/* let [spring, , fall, ] = ["봄", "여름", "가을", "겨울"]
-console.log(spring) */
-
-/* let [teacher, ...students] = ["kim", "Lee", "Park", "Choi"];
-console.log(teacher);
-console.log(students) */
-
-let x = 10;
-let y = 20;
-
-[x, y] = [y, x];
+let g3 = gen();
+g3.next();
+for (let i of g3) console.log(i);
