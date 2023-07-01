b78f61f64e527b383590f4f898e9f0c6a75eb96d
Her Donghyuk <idnosukja@naver.com>
2023-07-01 13:19:14 +0900

    230701

--- doit-js-main/13/js/00 js.js
+++ doit-js-main/13/js/00 js.js
@@ -1,48 +1,5 @@
-const pizza = new Promise((resolve, _) => {
-  resolve("주문합니다");
-});
-
-const step1 = (message) => {
-  console.log(message);
-  return new Promise((resolve, _) => {
-    setTimeout(() => {
-      resolve("도우완");
-    }, 1500);
+const pizza = () => {
+  return new Promise((resolve, reject) => {
+    resolve("주문완");
   });
 };
-
-const step2 = (message) => {
-  console.log(message);
-  return new Promise((resolve, _) => {
-    setTimeout(() => {
-      resolve("토핑완");
-    }, 1500);
-  });
-};
-
-const step3 = (message) => {
-  console.log(message);
-  return new Promise((resolve, _) => {
-    setTimeout(() => {
-      resolve("굽기완");
-    }, 1500);
-  });
-};
-
-/* pizza
-  .then(step1)
-  .then(step2)
-  .then(step3)
-  .then(console.log)
-  .then(() => {
-    console.log("가져가");
-  }); */
-
-pizza
-  .then((result) => step1(result))
-  .then((result) => step2(result))
-  .then((result) => step3(result))
-  .then((result) => console.log(result))
-  .then(() => {
-    console.log("가져가");
-  });
