a8d482a11bf7385996b6678844a60e2ee95c9564
HerDonghyuk <idnosukja@naver.com>
2023-07-05 16:32:26 +0900

    230705

--- doit-js-main/13/js/00 js.js
+++ doit-js-main/13/js/00 js.js
@@ -1,37 +1,41 @@
-/* fetch(quotesURL)
-  .then(response => response.json())
-  .then(data => {
-    const result = document.querySelector("#result");
-    const random = Math.floor(Math.random() * 30);  // 0 ~ 29 사이의 수
-    result.querySelector(".quote").innerHTML = data.quotes[random].quote;
-    result.querySelector(".author").innerHTML = ` - ${data.quotes[random].author}`;
-  })
-  .catch(error => console.log(error)); */
+const pizza = () => {
+  return new Promise((resolve, reject) => {
+    resolve("주문완");
+  });
+};
 
-/* async function init() {
-  try {
-    const response = await fetch("http://dummyjson.com/quotes");
-    const data = await response.json();
-    const result = document.querySelector("#result");
-    const random = Math.floor(Math.random() * 30);
-    result.querySelector(".quote").innerHTML = data.quotes[random].quote;
-    result.querySelector(
-      ".author"
-    ).innerHTML = `- ${data.quotes[random].author}`;
-  } catch (error) {
-    console.log(error);
-  }
-}
+const step1 = (message) => {
+  console.log(message);
+  return new Promise((resolve, _) => {
+    setTimeout(() => {
+      resolve("도우완");
+    }, 500);
+  });
+};
 
-init(); */
+const step2 = (message) => {
+  console.log(message);
+  return new Promise((resolve, _) => {
+    setTimeout(() => {
+      resolve("토핑완");
+    }, 500);
+  });
+};
 
-async function init() {
-  try {
-    const response = await fetch("http://dummyjson.com/quotes");
-    const data = await response.json();
-    const result = document.querySelector("#result");
-    const random = Math.random() * 30;
-  } catch (error) {
-    console.log(error);
-  }
-}
+const step3 = (message) => {
+  console.log(message);
+  return new Promise((resolve, _) => {
+    setTimeout(() => {
+      resolve("굽기완");
+    }, 500);
+  });
+};
+
+pizza()
+  .then(step1)
+  .then(step2)
+  .then(step3)
+  .then(console.log)
+  .then(() => {
+    console.log("준비완");
+  });
