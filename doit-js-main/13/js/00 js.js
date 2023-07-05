cabe4a156007ff3738ccf738dea689a34387198e
HerDonghyuk <idnosukja@naver.com>
2023-07-05 14:27:18 +0900

    230705

--- doit-js-main/13/js/00 js.js
+++ doit-js-main/13/js/00 js.js
@@ -1,4 +1,37 @@
-async function displayHello() {
-  console.log("hello");
+/* fetch(quotesURL)
+  .then(response => response.json())
+  .then(data => {
+    const result = document.querySelector("#result");
+    const random = Math.floor(Math.random() * 30);  // 0 ~ 29 사이의 수
+    result.querySelector(".quote").innerHTML = data.quotes[random].quote;
+    result.querySelector(".author").innerHTML = ` - ${data.quotes[random].author}`;
+  })
+  .catch(error => console.log(error)); */
+
+/* async function init() {
+  try {
+    const response = await fetch("http://dummyjson.com/quotes");
+    const data = await response.json();
+    const result = document.querySelector("#result");
+    const random = Math.floor(Math.random() * 30);
+    result.querySelector(".quote").innerHTML = data.quotes[random].quote;
+    result.querySelector(
+      ".author"
+    ).innerHTML = `- ${data.quotes[random].author}`;
+  } catch (error) {
+    console.log(error);
+  }
 }
-displayHello();
+
+init(); */
+
+async function init() {
+  try {
+    const response = await fetch("http://dummyjson.com/quotes");
+    const data = await response.json();
+    const result = document.querySelector("#result");
+    const random = Math.random() * 30;
+  } catch (error) {
+    console.log(error);
+  }
+}
